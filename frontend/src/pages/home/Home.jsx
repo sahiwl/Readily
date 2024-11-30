233602729fc0a9c9a38ba0174f652b84b6674cff
sahiwl <piyushkrray1@gmail.com>
2024-11-30 20:28:00 +0530

    added banner section

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Banner } from './Banner'
+import { TopSellers } from './TopSellers'
+
+export const Home = () => {
+  return (
+    <div>
+        <Banner/>
+        <TopSellers/>
+    </div>
+  )
+}
---
+++ frontend/src/pages/home/TopSellers.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TopSellers = () => {
+  return (
+    <div>TopSellers</div>
+  )
+}
