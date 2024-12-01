5017970040d79d151acaa7c5d45b45d3a2551621
sahiwl <piyushkrray1@gmail.com>
2024-12-01 14:17:57 +0530

    📢: added news.jsx, recommended.jsx, homepage ready

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Banner } from './Banner'
 import { TopSellers } from './TopSellers'
+import { Recommended } from './Recommended'
+import { News } from './News'
 
 export const Home = () => {
   return (
     <div>
         <Banner/>
         <TopSellers/>
+        <Recommended/>
+        <News/>
     </div>
   )
 }
---
+++ frontend/src/pages/home/Recommended.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Navigation, Pagination } from 'swiper/modules';
+import { BookCard } from '../books/BookCard';
+
+export const Recommended = () => {
+    const [books, setBooks] = useState([]);
+
+    
+
+    useEffect(()=>{
+        fetch("books.json").then(res=>res.json()).then((data)=>setBooks(data))
+    }, []);
+  return (
+    <>
+    <div className="py-16">
+
+        <h2 className='text-2xl font-semibold mb-6'>Recommended for you</h2>
+        <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        // pagination={{
+            //   clickable: true,
+            // }}
+            breakpoints={{
+                640: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 40,
+                },
+                1024: {
+                    slidesPerView: 2,
+                    spaceBetween: 50,
+                },
+                1180:{
+                    slidesPerView: 3,
+                    spaceBetween: 50,
+                }
+            }}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+            >
+
+{   books.length>0 && books.slice(8,18).map((book,index)=>(
+    <div className="">
+                         <SwiperSlide key={index}>
+                            <BookCard book={book}/>
+                            </SwiperSlide>
+                </div>
+                ))
+            }
+
+       
+       
+      </Swiper>
+            </div>
+    </>
+  )
+}
