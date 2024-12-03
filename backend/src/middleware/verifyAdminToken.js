6da6e17aed8fe978d284d8b41d43e421693c6fdc
sahiwl <piyushkrray1@gmail.com>
2024-12-03 22:58:35 +0530

    feat🛠️: admin can add, remove or edit books

--- backend/src/middleware/verifyAdminToken.js
+++ backend/src/middleware/verifyAdminToken.js
@@ -4,13 +4,13 @@ const JWT_SECRET = process.env.JWT_SECRET_KEY
 const verifyAdminToken = (req,res,next)=>{
     const token = req.headers['authorization']?.split(' ')[1];
 
-    console.log(token)
+    // console.log(token)
     if(!token){
         return res.status(401).json({ message: "Access Denied. No token provided."})
     }
     jwt.verify(token, JWT_SECRET, (err,user)=>{
         if(err){
-            return res.stats(403).json({message: "Invalid credentials"})
+            return res.status(403).json({message: "Invalid credentials"})
         }
         req.user = user;
         next(); 
