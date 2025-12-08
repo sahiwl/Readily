e5ebb5809f0f4545a4d9b393d4841f8561df810b
sahiwl <piyushkrray1@gmail.com>
2025-12-08 23:35:46 +0530

    update dependencies

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,6 @@ app.get('/', (req, res) => {
 
 async function main() {
     await mongoose.connect(process.env.DB_URL);
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 main().then(() => console.log("MongoDB connected successfully")).catch(err => console.log(err));
